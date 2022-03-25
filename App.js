5b7a753bfa4a02bd1be1753b9e7c5f4452345389
alishdemo <92447692+alishdemo@users.noreply.github.com>
2022-03-25 07:45:50 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import {View,Text,Button,StyleSheet} from 'react-native';
 import AppHeader from './components/AppHeader';
-import HomeScreen from './Screens/HomeScreen';
-import SummaryScreen from './Screens/SummaryScreen';
+import HomeScreen from './screens/HomeScreen';
+import SummaryScreen from './screens/SummaryScreen';
 import {createAppContainer, createSwitchNavigator}from 'react-navigation';
 export default class App extends React.Component{
 
